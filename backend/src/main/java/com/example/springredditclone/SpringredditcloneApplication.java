abdb366cc125472ab5b66d607e5aab0044df07cb
wslmacieira <wslmacieira@outlook.com>
2021-10-01 00:44:24 -0300

    Document REST API using Swagger & Springfox

--- backend/src/main/java/com/example/springredditclone/SpringredditcloneApplication.java
+++ backend/src/main/java/com/example/springredditclone/SpringredditcloneApplication.java
@@ -2,10 +2,13 @@ package com.example.springredditclone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
+import com.example.springredditclone.config.SwaggerConfiguration;
 
 @SpringBootApplication
 @EnableAsync
+@Import(SwaggerConfiguration.class)
 public class SpringredditcloneApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/example/springredditclone/config/SecurityConfig.java
+++ backend/src/main/java/com/example/springredditclone/config/SecurityConfig.java
@@ -39,6 +39,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/subreddit")
                 .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/posts/**")
+                .permitAll()
+                .antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**")
+                .permitAll()
                 .anyRequest()
                 .authenticated();
                 httpSecurity.addFilterBefore(jwtAuthenticationFilter,
